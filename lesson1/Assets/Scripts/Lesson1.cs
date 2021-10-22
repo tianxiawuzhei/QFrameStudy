0a3d861f23f62b43985f8a9f4683ce528a4d6b7a
zhyq <1064639871@qq.com>
2021-10-22 11:18:48 +0800

    lesson1 log hello world

---
+++ lesson1/Assets/Scripts/Lesson1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using QFramework;
+using UnityEngine;
+
+public class Lesson1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Log.I("hello world");
+        "lesson1 start".LogInfo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
