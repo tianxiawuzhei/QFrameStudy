9c63e2ce817b637b12652db1133c2e46986f9106
zhyq <1064639871@qq.com>
2021-10-22 14:05:10 +0800

    ViewController + Bind  测试

---
+++ lesson1/Assets/Scripts/Lesson2ViewCtr.Designer.cs
@@ -0,0 +1,14 @@
+// Generate Id:27932550-b5c8-4070-adbd-570496db076e
+using UnityEngine;
+
+// 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
+// 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
+namespace QFStudyLesson
+{
+	public partial class Lesson2ViewCtr
+	{
+		public UnityEngine.Transform Player;
+		public UnityEngine.Transform Level;
+
+	}
+}
---
+++ lesson1/Assets/Scripts/lesson2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lesson2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var player = transform.Find("Player").gameObject;
+    }
+}
